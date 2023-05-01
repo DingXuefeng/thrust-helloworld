0420ed4401b68f4c4b26055a0c920bac80a6b76c
Xuefeng Ding <dingxf@ihep.ac.cn>
2023-05-01 11:06:19 +0800

    more about lambda capture of *this

--- naive.cu
+++ naive.cu
@@ -10,6 +10,7 @@
 #include <thrust/transform_reduce.h>
 #include <thrust/iterator/counting_iterator.h>
 #include <thrust/functional.h>
+#include <thrust/device_vector.h>
 #include <iostream>
 #ifdef USE_FLOAT
 using real_t = float;
@@ -18,18 +19,29 @@ using real_t = double;
 #endif
 
 struct op {
+    op(real_t _a,real_t _b) : a(_a),b(_b),c(2),d(thrust::raw_pointer_cast(c.data())) {
+        std::cout<<"in op()"<<std::endl;
+        c = {1,2};
+    }
     void run() {
+        auto oa = this->a;
+        auto ob = this->b;
+        auto od = this->d;
+        // better not to capture *this. 
+        // at least cuda-12.1 will copy **everthing**, not optimized.
         auto sum = thrust::transform_reduce(
             thrust::counting_iterator<int>(0),
             thrust::counting_iterator<int>(100),
-            [*this] __device__(const int x) -> real_t
+            [oa,ob,od] __device__(const int x) -> real_t
             {
-                return x * a + b;
+                return (x * oa + ob)*od[0]+od[1];
             },
             0., thrust::plus<real_t>());
         std::cout << "result: " << sum << std::endl;
     }
     const real_t a,b;
+    thrust::device_vector<real_t> c;
+    real_t *d;
 };
 
 int main() {
